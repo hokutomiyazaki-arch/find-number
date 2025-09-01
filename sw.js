87eb4f1ff99240849c34c469e480b833140c35b4
Functional Neuro Training <hokutomiyazaki312@gmail.com>
2025-09-01 20:48:42 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,70 +1,21 @@
-const CACHE_NAME = 'speed-touch-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './FNT512.png',
-  './manifest.json'
-];
-
-// インストール時にキャッシュ
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-  self.skipWaiting();
-});
-
-// フェッチ時にキャッシュから返す
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // キャッシュがあればそれを返す
-        if (response) {
-          return response;
-        }
-        
-        // なければネットワークから取得
-        return fetch(event.request).then(
-          (response) => {
-            // 有効なレスポンスでない場合はそのまま返す
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-            
-            // レスポンスをクローンしてキャッシュに保存
-            const responseToCache = response.clone();
-            
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-            
-            return response;
-          }
-        );
-      })
-  );
-});
-
-// 古いキャッシュを削除
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './FNT512.png',
+        './FNT512-transparent.png',
+        './manifest.json'
+      ]);
     })
   );
-  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
